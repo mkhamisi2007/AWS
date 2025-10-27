193470a7bcd007d383491e5fc369632d5179569b
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-27 17:20:57 +0100

    Update Cache.cmd

--- API-GateWay/Cache.cmd
+++ API-GateWay/Cache.cmd
@@ -16,3 +16,9 @@ aws apigateway update-stage \
   --rest-api-id a1b2c3d4 \
   --stage-name prod \
   --patch-operations op=replace,path=/cacheClusterSize,value=1.6
+
+--------------delete cache ---------------------
+aws apigateway flush-stage-cache \
+  --rest-api-id a1b2c3d4 \
+  --stage-name prod
+
