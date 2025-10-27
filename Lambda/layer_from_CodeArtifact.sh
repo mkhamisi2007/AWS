11cc7b50ddd818abcab8a9481c9d743c7b8d01fd
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-27 09:13:29 +0100

    Create layer_from_CodeArtifact.sh

---
+++ Lambda/layer_from_CodeArtifact.sh
@@ -0,0 +1,17 @@
+aws codeartifact login --tool pip --repository pypi-store --domain my-domain --domain-owner 277707094115 --region us-east-1
+
+--------------------------add layers in lambda  -------------------------
+REGION=us-east-1
+DOMAIN=my-domain
+REPO=pypi-store
+TOKEN=$(aws codeartifact get-authorization-token \
+  --domain $DOMAIN --region $REGION \
+  --query authorizationToken --output text)
+ENDPOINT=$(aws codeartifact get-repository-endpoint \
+  --domain $DOMAIN --repository $REPO --format pypi \
+  --region $REGION --query repositoryEndpoint --output text)
+HOST=${ENDPOINT#https://}
+INDEX_URL="https://aws:${TOKEN}@${HOST}simple/"
+pip install --index-url "$INDEX_URL" -t python/ mangum asyncpg python-dotenv
+zip -r layer-fastapi-ca.zip python
+aws lambda publish-layer-version   --layer-name fastapi-deps-from-codeartifact   --zip-file fileb://layer-fastapi-ca.zip   --compatible-runtimes python3.12   --region $REGION
