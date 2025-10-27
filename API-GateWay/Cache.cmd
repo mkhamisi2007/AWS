b874bfc2185904f014e32f2fe6215e4720e10786
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-27 17:11:03 +0100

    Create Cache.cmd

---
+++ API-GateWay/Cache.cmd
@@ -0,0 +1,18 @@
+----------- enable cache ------------
+aws apigateway update-stage \
+  --rest-api-id a1b2c3d4 \
+  --stage-name prod \
+  --patch-operations op=replace,path=/cacheClusterEnabled,value=true
+
+----------- for a methode ------------
+  aws apigateway update-method \
+  --rest-api-id a1b2c3d4 \
+  --resource-id xyz123 \
+  --http-method GET \
+  --patch-operations op=replace,path=/cachingEnabled,value=true
+
+---------------- change capacity --------------
+  aws apigateway update-stage \
+  --rest-api-id a1b2c3d4 \
+  --stage-name prod \
+  --patch-operations op=replace,path=/cacheClusterSize,value=1.6
