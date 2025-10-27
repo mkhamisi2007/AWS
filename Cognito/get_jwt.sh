1dde60909518efe518be81a87ebfc596f1400f7b
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-27 11:40:31 +0100

    Create get_jwt.sh

---
+++ Cognito/get_jwt.sh
@@ -0,0 +1,13 @@
+export CLIENT_ID="53o498lkiygfdhlq6olo4el85d1kcm"
+export CLIENT_SECRET="1abfs2dbi4tgue1rcj_èjkhfddghtdjgu7lh7f94cgrri"
+export USERNAME="mohammad.khamisi.etu@univ-lille.fr"
+SECRET_HASH=$(printf "%s" "$USERNAME$CLIENT_ID" | openssl dgst -sha256 -hmac "$CLIENT_SECRET" -binary | base64)
+echo "$SECRET_HASH"
+-----
+aws cognito-idp initiate-auth \
+  --region us-east-1 \
+  --client-id "$CLIENT_ID" \
+  --auth-flow USER_PASSWORD_AUTH \
+  --auth-parameters USERNAME="$USERNAME",PASSWORD='YourPasswordHere',SECRET_HASH="$SECRET_HASH"
+---- out put ---------------------
+ "AccessToken": "eyJraWQiOiI5UDBuMEpFMkczVkJFYXcr......
