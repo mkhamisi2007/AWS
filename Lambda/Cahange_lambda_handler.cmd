23df75c7cea7c0f7e3e1a6bea0c4e24157c50313
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-27 06:33:33 +0100

    Delete Lambda/Cahange_lambda_handler.cmd

--- Lambda/Cahange_lambda_handler.cmd
+++
@@ -1,6 +0,0 @@
-aws lambda update-function-configuration \
-    --function-name MyLambdaFunction \
-    --handler app.my_entry_point
-
-------------------------------------
-aws lambda get-function-configuration --function-name MyLambdaFunction --query "Handler"
