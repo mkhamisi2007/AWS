5e05f40f674fc6c32a51e831a8e5034e096050a4
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-28 13:04:21 +0100

    Update Run-Task-EventBridge.sh

--- ECS/Run-Task-EventBridge.sh
+++ ECS/Run-Task-EventBridge.sh
@@ -1,1 +1,23 @@
-
+aws events put-rule \
+  --name RunECSTaskEvery5Min \
+  --schedule-expression "rate(5 minutes)"
+-------------------------
+aws events put-targets \
+  --rule RunECSTaskEvery5Min \
+  --targets '[
+    {
+      "Id": "RunECSTask",
+      "Arn": "arn:aws:ecs:us-east-1:123456789012:cluster/my-ecs-cluster",
+      "RoleArn": "arn:aws:iam::123456789012:role/EventBridgeInvokeEcsRole",
+      "EcsParameters": {
+        "LaunchType": "FARGATE",
+        "TaskDefinitionArn": "arn:aws:ecs:us-east-1:123456789012:task-definition/my-task:1",
+        "NetworkConfiguration": {
+          "awsvpcConfiguration": {
+            "Subnets": ["subnet-abc123"],
+            "AssignPublicIp": "ENABLED"
+          }
+        }
+      }
+    }
+  ]'
