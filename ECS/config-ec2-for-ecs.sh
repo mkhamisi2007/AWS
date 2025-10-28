7d7b6e2db0cb1760f053abdffdb91d1d435ca725
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-28 09:12:02 +0100

    Update config-ec2-for-ecs.sh

--- ECS/config-ec2-for-ecs.sh
+++ ECS/config-ec2-for-ecs.sh
@@ -1,6 +1,20 @@
 aws ecs create-cluster --cluster-name my-ecs-cluster
 ------- prepare instance(ubuntu)------------------
+# role permission => AmazonEC2ContainerServiceforEC2Role
 sudo apt update -y
 sudo apt install -y docker.io
 sudo systemctl enable docker
 sudo systemctl start docker
+
+sudo mkdir -p /etc/ecs
+echo "ECS_CLUSTER=my-ecs-cluster" | sudo tee /etc/ecs/ecs.config
+sudo docker run --name ecs-agent --detach \
+  --restart=always \
+  --volume=/var/run/docker.sock:/var/run/docker.sock \
+  --volume=/var/log/ecs/:/log \
+  --volume=/var/lib/ecs/data:/data \
+  --volume=/etc/ecs:/etc/ecs \
+  --net=host \
+  --env-file=/etc/ecs/ecs.config \
+  amazon/amazon-ecs-agent:latest
+
