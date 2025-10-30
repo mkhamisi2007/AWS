7b06303a1cf4f37823020b84dfc14188905e78ff
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-30 06:35:49 +0100

    Update Replication.cmd

--- RDS/Replication.cmd
+++ RDS/Replication.cmd
@@ -7,3 +7,16 @@ aws rds delete-db-instance --db-instance-identifier mydb-replica1
 --------- Promote replica ---------------
 aws rds promote-read-replica \
   --db-instance-identifier my-read-replica
+--------- Multi AZ (Disaster Recovery)---------------
+aws rds create-db-instance \
+  --db-instance-identifier prod-db \
+  --multi-az \
+  --engine mysql \
+  --master-username admin \
+  --master-user-password MyPassword123
+------OR enable for existing database-------
+aws rds modify-db-instance \
+  --db-instance-identifier prod-db \
+  --multi-az \
+  --apply-immediately
+
