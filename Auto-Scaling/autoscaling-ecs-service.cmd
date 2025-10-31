19674ccdf43a4a2f615534df4b56198c08ebe9fb
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-31 09:47:46 +0100

    Delete Auto-Scaling/autoscaling-ecs-service.cmd

--- Auto-Scaling/autoscaling-ecs-service.cmd
+++
@@ -1,6 +0,0 @@
-aws application-autoscaling register-scalable-target \
-  --service-namespace ecs \
-  --resource-id service/my-cluster/my-service \
-  --scalable-dimension ecs:service:DesiredCount \
-  --min-capacity 1 \
-  --max-capacity 10
