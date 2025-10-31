d37b3fc1a9dc63c3bdaec0cf304eb9b769d9e2f8
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-31 09:42:34 +0100

    Create autoscaling-ecs-service.cmd

---
+++ Auto-Scaling/autoscaling-ecs-service.cmd
@@ -0,0 +1,6 @@
+aws application-autoscaling register-scalable-target \
+  --service-namespace ecs \
+  --resource-id service/my-cluster/my-service \
+  --scalable-dimension ecs:service:DesiredCount \
+  --min-capacity 1 \
+  --max-capacity 10
