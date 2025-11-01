bff3a5cb33091b058fc1ad802c08e700690d7adc
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-11-01 11:30:49 +0100

    Update get-md5-checksum.cmd

--- S3/get-md5-checksum.cmd
+++ S3/get-md5-checksum.cmd
@@ -1,5 +1,14 @@
+-------------MD5 algorithm--------------------------
 aws s3api head-object \
   --bucket my-bucket \
   --key myfile.txt \
   --query ETag \
   --output text
+
+--------------Other algorithm-------------------
+aws s3api put-object \
+  --bucket my-bucket \
+  --key myfile.txt \
+  --body myfile.txt \
+  --checksum-algorithm SHA256
+
