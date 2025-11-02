c5e8d5697e713457737729f26cf96f9a76dde95a
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-11-02 06:57:33 +0100

    Create aggregator.cmd

---
+++ AWS-Config/aggregator.cmd
@@ -0,0 +1,3 @@
+aws configservice put-configuration-aggregator \
+  --configuration-aggregator-name OrgAggregator \
+  --organization-aggregation-source OrganizationArn=arn:aws:organizations::123456789012:organization/o-abc123
