6c1249a9c9e605e60a1a1f37c9cd458cbe512c6d
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-11-02 07:20:02 +0100

    Create Disable-AWS-Config.cmd

---
+++ AWS-Config/Disable-AWS-Config.cmd
@@ -0,0 +1,8 @@
+aws configservice stop-configuration-recorder \
+  --configuration-recorder-name default
+
+aws configservice delete-configuration-recorder \
+  --configuration-recorder-name default
+
+aws configservice delete-delivery-channel \
+  --delivery-channel-name default
