8fa336da64e3ca761d926a0e90e3491bc18904d4
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-01-22 09:10:24 +0100

    Update Install-eksctl.sh

--- EKS/Install-eksctl.sh
+++ EKS/Install-eksctl.sh
@@ -6,3 +6,12 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 choco install eksctl -y
 eksctl version
 eksctl get clusters
+---------------------------
+eksctl create cluster \
+  --name test-cluster \
+  --version 1.17 \
+  --region eu-central-1 \
+  --nodegroup-name linux-nodes \
+  --node-type t2.micro \
+  --nodes 2
+
