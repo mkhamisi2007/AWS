9790128d5e5ba9525451b4f801cc2eb15bf99517
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-01-29 09:11:22 +0100

    Update make-an-AMI.sh

--- Packer/make-an-AMI.sh
+++ Packer/make-an-AMI.sh
@@ -12,3 +12,45 @@ packer version
 packer init .
 packer validate .
 packer build .
+----------------------main.pkr.hcl--------------------
+packer {
+  required_plugins {
+    amazon = {
+      source  = "github.com/hashicorp/amazon"
+      version = ">= 1.0.0"
+    }
+  }
+}
+
+source "amazon-ebs" "ubuntu" {
+  region        = "eu-west-1"
+  instance_type = "t3.micro"
+  ssh_username  = "ubuntu"
+  ami_name      = "demo-ubuntu-{{timestamp}}"
+
+  source_ami_filter {
+    filters = {
+      name                = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
+      virtualization-type = "hvm"
+      root-device-type    = "ebs"
+    }
+    owners      = ["099720109477"]
+    most_recent = true
+  }
+}
+
+build {
+  sources = ["source.amazon-ebs.ubuntu"]
+
+  provisioner "shell" {
+    script = "scripts/install.sh"
+  }
+}
+-----------------scripts/install.sh-------------------
+#!/bin/bash
+set -e
+
+sudo apt update
+sudo apt install -y nginx docker.io
+sudo systemctl enable docker
+
